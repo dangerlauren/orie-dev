1550c41584db13aeeee22ee0e38b4e5fef117233
Grant <grantlauren@wireless-10-145-66-88.public.utexas.edu>
2016-07-25 09:04:06 -0500

    adding all files

---
+++ index.php
@@ -0,0 +1,73 @@
+<?php
+// No direct access.
+defined('_JEXEC') or die;
+
+jimport('joomla.filesystem.file');
+
+JHtml::_('bootstrap.framework');
+JHtml::_('behavior.framework', true);
+JHtml::_('jquery.framework');
+
+// get params
+
+$app        = JFactory::getApplication();
+$doc        = JFactory::getDocument();
+$templateparams    = $app->getTemplate(true)->params;
+$menu = $app->getMenu()->getActive();
+if (is_object($menu)) :
+	if ($menu->params->get('pageclass_sfx')) :
+		$pageclass = ' class="'.$menu->params->get('pageclass_sfx').'"';
+	else :
+		$pageclass = NULL;
+	endif;
+endif;
+$doc->addStyleSheet('https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,800,700,600,300', $type = 'text/css', $media = 'all');
+$doc->addStyleSheet($this->baseurl.'/templates/'.$this->template.'/css/main.css', $type = 'text/css', $media = 'all');
+//$doc->addStyleSheet($this->baseurl.'/templates/'.$this->template.'/css/desk.css', $type = 'text/css', $media = 'all and (min-width: 1000px)');
+$doc->addStyleSheet($this->baseurl.'/templates/'.$this->template.'/css/tablet.css', $type = 'text/css', $media = 'all and (max-width: 768px)');
+//$doc->addStyleSheet($this->baseurl.'/templates/'.$this->template.'/css/tablet-small.css', $type = 'text/css', $media = 'all and (max-width: 680px)');
+$doc->addStyleSheet($this->baseurl.'/templates/'.$this->template.'/css/mobile.css', $type = 'text/css', $media = 'all and (max-width: 400px)');
+$doc->addScript($this->baseurl.'/templates/'.$this->template.'/javascript/orie.js', $type = 'text/javascript');
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>" >
+<head>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1"/>
+	<jdoc:include type="head" />
+</head>
+<body<?php echo $pageclass;?>>
+  <?php include_once("analyticstracking.php") ?>
+	<div id="wrapper">
+		<div id="mast">
+			<jdoc:include type="modules" name="mast" style="none" />
+		</div>
+		<div id="header">
+			<jdoc:include type="modules" name="header" style="none" />
+		</div>
+		<div id="left">
+			<jdoc:include type="modules" name="left" style="none" />
+		</div>
+		<div id="position-1">
+			<jdoc:include type="modules" name="position-1" style="none" />
+		</div>
+		<div id="position-2">
+			<jdoc:include type="modules" name="position-2" style="none" />
+		</div>
+		<div id="position-3">
+			<jdoc:include type="modules" name="position-3" style="none" />
+		</div>
+		<div id="position-4">
+			<jdoc:include type="modules" name="position-4" style="none" />
+		</div>
+		<div id="position-5">
+			<jdoc:include type="modules" name="position-5" style="none" />
+		</div>
+		<jdoc:include type="message" />
+		<jdoc:include type="component" />
+		<div id="footer">
+			<jdoc:include type="modules" name="footer" style="none" />
+		</div>
+	</div>
+</body>
+</html>
---
+++ javascript/orie.js
@@ -0,0 +1,85 @@
+(function($) {
+
+	$(function(){
+		$('.custom-orielogo, .custom-cselogo, .custom-utlogo').click(function(){
+			window.location=$(this).find("a").attr("href");
+			return false;
+		});
+	});
+
+	$(function() {
+		$(".moduletable-mainnav > .menu > li.deeper, #left > .menu-subnav > li.deeper").mouseenter(function(){
+			var navclass = $(this).attr("class");
+		  $(this).addClass("showme");
+		}).mouseleave(function(){
+			var navclass = $(this).attr("class");
+		  $(this).removeClass("showme");
+		});
+	});
+
+	$(function(){
+		$( '.menuhead h3' ).click(function(){
+			$('#left .menu-subnav').slideToggle();
+		});		
+	});
+
+	$(function(){
+		$('.moduletable-mobilenav').click(function(){
+			$('#submast .menu-mobile').slideToggle().toggleClass('expand');
+		})
+	});
+
+	$(function(){
+		$( '.moduletable-mobilenav h3' ).click(function(){
+			$('.moduletable-mobilenav h3').css({'z-index': '999'});
+			var docHeight = $(document).height();
+			$("body").append("<div id='overlay'></div>");
+			$('#mast .menu-mobile').slideToggle();
+			$("#overlay").height(docHeight); 
+		   	$('#overlay').toggleClass('show');
+		});		
+	});
+
+	
+
+	$(function(){
+		$( '.moduletable-flexcontrols p a.next, .moduletable-flexcontrols p a.prev' ).click(function(event){
+			var n = $(this).attr('href').substring(1);
+			if (n == 'null') {
+				return false;
+			} else {
+				$('.'+ n).css({'display':'block'});
+				$('.moduletable-flex:not(.'+ n +')').css({'display':'none'});
+				switch (n) {
+					case "one" :
+						$('.moduletable-flexcontrols p a.next').attr('href', '#two');
+						$('.moduletable-flexcontrols p a.prev').attr('href','#null').addClass('disabled');
+						break;
+					case "two" :
+						$('.moduletable-flexcontrols p a.next').attr('href', '#three').removeClass('disabled');
+						$('.moduletable-flexcontrols p a.prev').attr('href','#one').removeClass('disabled');
+						break;
+					case "three" :
+						$('.moduletable-flexcontrols p a.next').attr('href','#null').addClass('disabled');
+						$('.moduletable-flexcontrols p a.prev').attr('href','#two');
+						break;
+					}
+			}
+		});		
+	});
+
+	$(function() {
+
+		$(".switch a").click(function(e) { 
+
+			var link = $(this).attr("href");
+			
+			$(link).slideToggle();
+			$(this).toggleClass('active');
+			
+			e.preventDefault(); 
+
+		});
+	});
+
+})(jQuery);
